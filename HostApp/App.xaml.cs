c4e57f44434fd5d20bbe372899fdd3e4f4624c9b
Seth Fuller <sfuller01@beckman.com>
2025-09-10 13:09:51 -0400

    Create simple .NET auto update issue

---
+++ HostApp/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HostApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        this.DispatcherUnhandledException += (s, e) =>
+        {
+            MessageBox.Show(
+                $"Unhandled exception:\n{e.Exception.GetType().Name}\n{e.Exception.Message}",
+                ".NET Update Repro");
+            e.Handled = true;
+        };
+    }
+}
+
---
+++ HostApp/Page1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HostApp
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        private readonly NavigationService _nav;
+
+        public Page1(Frame host)
+        {
+            InitializeComponent();
+            _nav = host.NavigationService;
+        }
+
+        private void OnGo(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _nav.Navigate(new Page2());
+        }
+    }
+}
